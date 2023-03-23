6f54ad8a93c2e25143d7f3b63256f9bba9fe280d
thejam2 <thejam2@naver.com>
2023-03-23 10:39:57 +0000

    useInput

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import { useState } from "react";
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  // validator 추가
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value); // validator 실행
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+
+  return { value, onChange };
+};
+
 export default function App() {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+
+  const maxLen = (value) => value.length < 10; // maxLen 함수 선언
+  const name = useInput("hj", maxLen); // maxLen을 args로 전달
   return (
     <div className="App">
       <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={incrementItem}>Increment</button>
       <button onClick={decrementItem}>Decrement</button>
+      <input placeholder="name" {...name} />
     </div>
   );
 }

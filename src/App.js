3f5739c7367865483f380860074c37573593f23c
thejam2 <thejam2@naver.com>
2023-03-23 10:34:55 +0000

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 }

01f134418bd0271c6bf3899e94f9fb16483b09be
thejam2 <thejam2@naver.com>
2023-03-29 08:09:34 +0000

    useBeforeLeave

--- src/App.js
+++ src/App.js
@@ -108,6 +108,23 @@ const usePreventLeave = () => {
   return { enablePrevent, disablePrevent }; // 두 함수를 return
 };
 
+//useBeforeLeave
+const useBeforeLeave = onBefore => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = event => {
+    const { clientY } = event; // event : MouseEvent의 객체
+    if (clientY <= 0) { // clientY(마우스의 좌표값)가 0 이하
+      onBefore();
+    }
+  };
+  useEffect(() => { 
+    document.addEventListener("mouseleave", handle); // 컴포넌트가 mount 되면, mouseleave 이벤트 생성
+    return () => document.removeEventListener("mouseleave", handle); // 컴포넌트가 unmount 되면, mouseleave 이벤트 제거
+  }, []); // 이벤트가 document에 추가 되는 것을 막음(한번만 실행)
+};
+
 export default function App() {
   //
   const [item, setItem] = useState(1);
@@ -139,6 +156,10 @@ export default function App() {
 
   //usePreventLeave
   const { enablePrevent, disablePrevent } = usePreventLeave();
+  
+  //useBeforeLeave
+  const dontLeave = () => console.log("Pls dont leave");
+  useBeforeLeave(dontLeave);
 
   return (
     <div className="App">
